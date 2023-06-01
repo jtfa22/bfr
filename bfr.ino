18fcff0be726b95d751991025549dd03ad2c143c
James Tseng <22jtseng@gmail.com>
2023-06-01 10:40:38 -0700

    switch quaternion library and add prelim controller

--- bfr.ino
+++ bfr.ino
@@ -7,7 +7,7 @@
 #include <Servo.h>
 #include <Math.h>
 #include "SensorFusion.h"
-#include <Geometry.h>
+#include <quaternion_type.h>
 
 const int led = LED_BUILTIN;
 int ledIter=0;
@@ -26,14 +26,28 @@ String filename;
 enum bfr_state{IDLE,PAD,LAUNCH,FLIGHT};
 enum bfr_state state = PAD;
 
+// PD controller
+#define Kp 0.1
+#define Kd 0.01
+// mixer gains
+#define Lx 1
+#define Ly 1
+#define Lz 1
+// servo lims
+#define contr_min -100
+#define contr_max 100
+#define servo_min 1500 // microsec
+#define servo_max 1900
+
 // q [1 0 0 0] is flat, starside down facing North (NED)
-Geometry::Quaternion q_set = {0,0,0,1};
+quat_t q_set = {1,0,0,0};
 
 // AHRS
 float ax_0=0; float ay_0=0; float az_0=0;
-#define s_0v 50
+#define s_0v 128
 float gx_0v[s_0v]; float gy_0v[s_0v]; float gz_0v[s_0v]; uint8_t i_0v=0;
-#define s_rv 50 
+#define s_rv 32 
+float ax_rv[s_rv]; float ay_rv[s_rv]; float az_rv[s_rv];
 float gx_rv[s_rv]; float gy_rv[s_rv]; float gz_rv[s_rv]; uint8_t i_rv=0;
 float gx_r=0; float gy_r=0; float gz_r=0;
 float gx_0=0; float gy_0=0; float gz_0=0;
@@ -112,7 +126,7 @@ void loop() {
   // // // // // // // // //
   float pressure = ps.readPressureMillibars(); float altitude = ps.pressureToAltitudeMeters(pressure); float temperature = ps.readTemperatureC();
   float ax_r=imu.readFloatAccelX(); float ay_r=imu.readFloatAccelY(); float az_r=imu.readFloatAccelZ(); 
-  // float gx_r=imu.readFloatGyroX();  float gy_r=imu.readFloatGyroY();  float gz_r=imu.readFloatGyroZ();
+  ax_rv[i_rv]=ax_r; ay_rv[i_rv]=ay_r; az_rv[i_rv]=az_r;
   float ax=ax_r-ax_0; float ay=ay_r-ay_0; float az=az_r-az_0; 
   // smooth out noisy gyro measurements
   gx_rv[i_rv]=imu.readFloatGyroX(); gy_rv[i_rv]=imu.readFloatGyroY(); gz_rv[i_rv]=imu.readFloatGyroZ(); i_rv=(i_rv+1)%s_rv;
@@ -132,7 +146,7 @@ void loop() {
   ahrs.MadgwickUpdate(gx,gy,gz,ax,ay,az,mx,my,mz,dt);
 //  ahrs.MadgwickUpdate(gx,gy,gz,ax,ay,az,dt);
   float* q_r = ahrs.getQuat();
-  Geometry::Quaternion q_imu = {q_r[1],q_r[2],q_r[3],q_r[0]};
+  quat_t q_imu = quat_t(q_r).norm();
 
   // // // // // // // // //
   // STATE TRANSITION LOGIC
@@ -168,7 +182,26 @@ void loop() {
   // STATE FLIGHT
   // // // // // // // // //
   else if (state == FLIGHT) {
+    quat_t q_diff = q_set * q_imu.conj();
+    int8_t sgn = 1;
+    if (q_diff.get(0) < 0) { sgn = -1; }
     
+//    PD controller
+    float Mbx = - sgn * Kp * q_diff.get(1) - Kd * gx;
+    float Mby = - sgn * Kp * q_diff.get(2) - Kd * gy;
+    float Mbz = - sgn * Kp * q_diff.get(3) - Kd * gz;
+//    allcation
+    Mbx *= Lx; Mby *= Ly; Mbz *= Lz;
+    float sq3 = sqrt(3);
+    float L1 =        0 + 2*Mby + Mbz;
+    float L2 = -sq3*Mbx -   Mby + Mbz;
+    float L3 =  sq3*Mbx -   Mby + Mbz;
+//    fin mapping
+    uint16_t u1 = map(L1,contr_min,contr_max,servo_min,servo_max);
+    uint16_t u2 = map(L2,contr_min,contr_max,servo_min,servo_max);
+    uint16_t u3 = map(L3,contr_min,contr_max,servo_min,servo_max);
+//    servo output
+    fin1.writeMicroseconds(u1); fin2.writeMicroseconds(u2); fin3.writeMicroseconds(u3);
   }
 
   // // // // // // // // //
@@ -177,12 +210,12 @@ void loop() {
   if (millis()-time_print > 100) { time_print = millis();
 //    String ds = "ACC "+String(ax)+" "+String(ay)+" "+String(az)+" GYR "+String(gx)+" "+String(gy)+" "+String(gz)+" Q "+String(q0)+" "+String(q1)+" "+String(q2)+" "+String(q3);
 //    String ds = String(gx)+" "+String(gy)+" "+String(gz);//+" "+String(gx_r)+" "+String(gy_r)+" "+String(gz_r)+" "+String(gx_0)+" "+String(gy_0)+" "+String(gz_0);
-//    String ds = String(mag.m.x)+" "+String(mag.m.y)+" "+String(mag.m.z);
+    String ds = String(mag.m.x)+" "+String(mag.m.y)+" "+String(mag.m.z);
 //    String ds = String(ax)+" "+String(ay)+" "+String(az);
 //    String ds = String(gx_0)+" "+String(gy_0)+" "+String(gz_0);
 //    String ds = String(ahrs.getRoll())+" "+String(ahrs.getPitch())+" "+String(ahrs.getYaw());
 //    String ds = String(q_set.w())+" "+String(q_set.x())+" "+String(q_set.y())+" "+String(q_set.z());
-    String ds = String(q_imu.w())+" "+String(q_imu.x())+" "+String(q_imu.y())+" "+String(q_imu.z());
+//    String ds = String(q_imu.get(0))+" "+String(q_imu.get(1))+" "+String(q_imu.get(2))+" "+String(q_imu.get(3));
     Serial.println(ds);
   }
 
